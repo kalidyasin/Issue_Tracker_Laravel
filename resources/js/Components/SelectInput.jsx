f9eeaa6e7634f0100d021e3f0c9e2314d1158636
Nutoli12 <160708316+Nutoli12@users.noreply.github.com>
2024-08-08 18:00:17 +0300

    select input edited

--- resources/js/Components/SelectInput.jsx
+++ resources/js/Components/SelectInput.jsx
@@ -1,20 +1,25 @@
-import { Children, forwardRef, useRef } from 'react';
+import { forwardRef, useRef, useEffect } from 'react';
 
-export default forwardRef(function SelectInput({ className = '', isFocused = false,Children, ...props }, ref) {
-    const input = ref ? ref : useRef();
+export default forwardRef(function SelectInput({ className = '', isFocused = false, children, ...props }, ref) {
+    const inputRef = ref || useRef();
 
-   
+
+    useEffect(() => {
+        if (isFocused && inputRef.current) {
+            inputRef.current.focus();
+        }
+    }, [isFocused]);
 
     return (
         <select
             {...props}
-       
             className={
                 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm ' +
                 className
             }
-            ref={input}
-        > 
-        {Children}</select> 
+            ref={inputRef}
+        >
+            {children}
+        </select>
     );
 });
